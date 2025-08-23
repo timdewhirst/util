14ca7f0cd51b2cd3c1f4c3ea4becc0231a614d97
Tim Dewhirst <tim@bugless.co.uk>
2025-08-23 10:56:49 +0100

    use assert to check for valid arguments in constexpr

--- literals.hpp
+++ literals.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 // std
+#include <assert.h>
 #include <limits>
 #include <type_traits>
 
@@ -13,37 +14,37 @@ namespace util {
         typename T,
         typename = std::enable_if_t<std::is_integral_v<T>>
         >
-    T check_and_cast(unsigned long long int i)
+    constexpr T check_and_cast(unsigned long long int i)
     {
-        if ( i < std::numeric_limits<T>::lowest() || std::numeric_limits<T>::max() < i )
-            exception_builder<std::runtime_error>() << "argument is outside of valid range: " << i;
-
-        return (T)i;
+        return 
+            assert(i >= std::numeric_limits<T>::lowest()), 
+            assert(i <= std::numeric_limits<T>::max()),
+            (T)i;
     }
 }
 
 
-inline uint8_t operator ""_u8(unsigned long long int i)
+constexpr inline uint8_t operator ""_u8(unsigned long long int i)
 {
     return util::check_and_cast<uint8_t>(i);
 }
 
-inline uint16_t operator ""_u16(unsigned long long int i)
+constexpr inline uint16_t operator ""_u16(unsigned long long int i)
 {
     return util::check_and_cast<uint16_t>(i);
 }
 
-inline uint32_t operator ""_u32(unsigned long long int i)
+constexpr inline uint32_t operator ""_u32(unsigned long long int i)
 {
     return util::check_and_cast<uint32_t>(i);
 }
 
-inline uint64_t operator ""_u64(unsigned long long int i)
+constexpr inline uint64_t operator ""_u64(unsigned long long int i)
 {
     return util::check_and_cast<uint64_t>(i);
 }
 
-inline size_t operator""_sz(unsigned long long int i)
+constexpr inline size_t operator""_sz(unsigned long long int i)
 {
     return (size_t)i;
 }
